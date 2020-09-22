f70835e5d4a4a2d0306a595607e806bed120db33
Lukasz Matuszewski <lukasz.matuszewski@spartez-software.com>
2020-09-22 15:19:51 +0200

    init

---
+++ task-manager-backend/src/main/java/com/spartez/TakManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spartez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakManagerBackendApplication.class, args);
+	}
+
+}
---
+++ task-manager-backend/src/test/java/com/spartez/TakManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
