a1a4167b10b210c6fc28be577c9b9715fce5fcda
Łukasz Matuszewski <lukasz.matuszewski@appfire.com>
2021-08-17 18:58:40 +0200

    create issues with ids from the backend

--- task-manager-frontend/src/store/index.js
+++ task-manager-frontend/src/store/index.js
@@ -17,9 +17,11 @@ export default new Vuex.Store({
     CHANGE_COLUMN_TITLE(state, { columnIndex, newTitle }) {
       state.columns[columnIndex - 1].name = newTitle;
     },
-    CREATE_ISSUE(state, { title, description, columnIndex }) {
+    CREATE_ISSUE(state, {
+      id, title, description, columnIndex,
+    }) {
       state.columns[columnIndex - 1].issues.push({
-        id: 20, title, description,
+        id, title, description,
       });
     },
     DELETE_ISSUE(state, { fromColumnIndex, issueIndex }) {
@@ -35,16 +37,18 @@ export default new Vuex.Store({
       API.default.updateColumn(state.columns[columnIndex - 1]);
     },
     async loadState({ commit }) {
-      const response = API.default.fetchIssues();
+      const response = await API.default.fetchIssues();
       commit('LOAD_STATE', response.data);
     },
     deleteIssue({ commit }, { fromColumnIndex, issueIndex, issue }) {
       commit('DELETE_ISSUE', { fromColumnIndex, issueIndex });
       API.default.deleteIssue(issue.id);
     },
-    createIssue({ commit }, { title, description, columnIndex }) {
-      commit('CREATE_ISSUE', { title, description, columnIndex });
-      API.default.createIssue({ title, description, columnIndex });
+    async createIssue({ commit }, { title, description, columnIndex }) {
+      const { data: { id } } = await API.default.createIssue({ title, description, columnIndex });
+      commit('CREATE_ISSUE', {
+        id, title, description, columnIndex,
+      });
     },
     moveIssue({ commit }, {
       fromColumnIndex, toColumnIndex, issueIndex, issue,
