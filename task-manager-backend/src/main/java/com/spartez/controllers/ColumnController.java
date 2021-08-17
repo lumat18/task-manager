c4908dee45552536b09fabe2d8f43f9ab4152eb5
lumat18 <lumat@op.pl>
2021-08-17 19:40:06 +0200

    fix after reverting the changes

--- task-manager-backend/src/main/java/com/spartez/controllers/ColumnController.java
+++ task-manager-backend/src/main/java/com/spartez/controllers/ColumnController.java
@@ -17,6 +17,7 @@ import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin("http://localhost:8081")
 @RequestMapping("/api/column")
 public class ColumnController {
     private final ColumnService columnService;
--- task-manager-backend/src/main/java/com/spartez/controllers/IssueController.java
+++ task-manager-backend/src/main/java/com/spartez/controllers/IssueController.java
@@ -14,6 +14,7 @@ import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin("http://localhost:8081")
 @RequestMapping("/api/issue")
 public class IssueController {
     private final IssueService issueService;
